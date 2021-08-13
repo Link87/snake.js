b27122a5008164d2f84d4361e00a260dfda93e79
Marvin Gazibarić <m.gazibaric@live.de>
2021-08-13 23:19:51 +0200

    Switch to TS, Yarn and Webpack

---
+++ src/util/Direction.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines the directions snakes can walk in.
+ * @readonly
+ * @enum {number}
+ */
+export default class Direction {
+    static readonly NONE = new Direction('NONE', 0, 0);
+    static readonly UP = new Direction('UP', 0, -1);
+    static readonly RIGHT = new Direction('RIGHT', 1, 0);
+    static readonly DOWN = new Direction('DOWN', 0, 1);
+    static readonly LEFT = new Direction('LEFT', -1, 0);
+
+    // private to disallow creating other instances of this type
+    private constructor(private readonly key: string, public readonly dx: number, public readonly dy: number) {
+    }
+
+    toString() {
+        return this.key;
+    }
+}
